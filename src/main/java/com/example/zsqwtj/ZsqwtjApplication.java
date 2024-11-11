e50f104627a8610259178681feae2413f0e150a0
tengjiao.wang <tengjiao.wang@npctek.com>
2024-11-11 17:38:59 +0800

    first commit

---
+++ src/main/java/com/example/zsqwtj/ZsqwtjApplication.java
@@ -0,0 +1,13 @@
+package com.example.zsqwtj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsqwtjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsqwtjApplication.class, args);
+	}
+
+}
